7e5edd4ff77b877f61b9b003f93581f7e99d4c15
padsp@hotmail.com <padsp@hotmail.com>
2021-02-15 11:22:54 -0500

    Add project files.

---
+++ TestMVC/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using CodeChallengeMVC.Models;
+
+namespace TestMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            string Baseurl = "https://frontiercodingtests.azurewebsites.net/";
+
+            List<AccountModel> accountList = new List<AccountModel>();
+            
+            using( var APIClient = new HttpClient())
+            {
+                APIClient.BaseAddress = new Uri(Baseurl);
+                APIClient.DefaultRequestHeaders.Clear();
+                APIClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = await APIClient.GetAsync("api/accounts/getall");
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseString = response.Content.ReadAsStringAsync().Result;
+                    accountList = Newtonsoft.Json.JsonConvert.DeserializeObject<List<AccountModel>>(responseString);                   
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+            return View(accountList.OrderBy(row => row.PaymentDueDate != null).ThenBy(row => row.PaymentDueDate));
+        }
+    }
+}
---
+++ TestMVC/Models/AccountModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeChallengeMVC.Models
+{
+    public enum AccountStatuses
+    {
+        Active = 0,
+        Inactive = 1,
+        Overdue = 2
+    }
+
+    public class AccountModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public decimal AmountDue { get; set; }
+        public DateTime? PaymentDueDate { get; set; }
+        public AccountStatuses AccountStatusId { get; set; }
+    }
+}
